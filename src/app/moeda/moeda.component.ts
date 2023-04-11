9e691a6b267cfff355fc46c1565e068c1624a621
Ana <isabellacristinavieira2003@gmail.com>
2023-04-11 09:05:20 -0300

    Seconf Commit

--- src/app/moeda/moeda.component.ts
+++ src/app/moeda/moeda.component.ts
@@ -10,9 +10,28 @@ export class MoedaComponent {
   primeiroNumero: number = 0;
   segundoNumero: number = 0;
   resultado: number = 0;
+converter: any;
 
   limpar() {
     this.resultado = 0;
     this.segundoNumero = 0;
+
+    converter(); {
+      switch (this.selectedOption1) {
+        case '1':
+          this.segundoNumero = this.primeiroNumero * 1000;
+          break;
+        case '2':
+          this.segundoNumero = this.primeiroNumero / 5,20;
+          break;
+        default:
+          this.segundoNumero = 0;
+          break;
+      }
+    }
   }
 }
+function converter() {
+  throw new Error('Function not implemented.');
+}
+
