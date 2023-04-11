02b392b092e391d72962d7adf101888add6edcbd
Ana <isabellacristinavieira2003@gmail.com>
2023-04-11 10:21:11 -0300

    commit five

--- src/app/moeda/moeda.component.ts
+++ src/app/moeda/moeda.component.ts
@@ -10,19 +10,22 @@ export class MoedaComponent {
   primeiroNumero: number = 0;
   segundoNumero: number = 0;
   resultado: number = 0;
-converter: any;
+  converter: any;
 
-  limpar() {
-    this.primeiroNumero = 0;
-    this.segundoNumero = 0;
-    this.resultado = 0;
+   limpar() {
+      this.primeiroNumero = 0;
+      this.segundoNumero = 0;
+      this.resultado = 0;
 
-    converter(); {
-      this.resultado = this.primeiroNumero * 5.20;
-      return;
+        converter(); {
+         if(this.primeiroNumero !==0){
+         this.resultado = this.primeiroNumero * this.segundoNumero;
+         } else {
+             alert('Não é possível dividir por zero!');
+          }
       }
-    }
-  }
+   }
+}
 function converter() {
   throw new Error('Function not implemented.');
 }
